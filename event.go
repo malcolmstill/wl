c693a08d5d941d7141c6a42a55d3819dceaa7e09
Malcolm Still <malcolm.still@gmail.com>
2019-04-07 16:27:40 +0100

    Expose data (as Data) return Event rather than *Event

--- event.go
+++ event.go
@@ -11,7 +11,7 @@ import (
 type Event struct {
 	Pid    ProxyId
 	Opcode uint32
-	data   []byte
+	Data   []byte
 	scms   []syscall.SocketControlMessage
 	off    int
 	oob    []byte
@@ -75,7 +75,7 @@ func (c *Context) readEvent() (*Event, error) {
 	if n != int(size)-8 {
 		return nil, fmt.Errorf("Invalid message size.")
 	}
-	ev.data = data
+	ev.Data = data
 
 	bytePool.Give(buf)
 	bytePool.Give(control)
@@ -83,27 +83,28 @@ func (c *Context) readEvent() (*Event, error) {
 	return ev, nil
 }
 
-func ReadEventUnix(fd int) (*Event, error) {
+func ReadEventUnix(fd int) (Event, error) {
 	buf := bytePool.Take(8)
 	control := bytePool.Take(24)
 
 	n, oobn, _, _, err := unix.Recvmsg(fd, buf, control, unix.MSG_DONTWAIT)
 	if err != nil {
-		return nil, err
+		return Event{}, err
 	}
 	if n != 8 {
-		return nil, fmt.Errorf("Unable to read message header.")
+		return Event{}, fmt.Errorf("Unable to read message header.")
 	}
 
-	ev := new(Event)
+	// ev := new(Event)
+	var ev Event
 
 	if oobn > 0 {
 		if oobn > len(control) {
-			return nil, fmt.Errorf("Unsufficient control msg buffer")
+			return Event{}, fmt.Errorf("Unsufficient control msg buffer")
 		}
 		scms, err := syscall.ParseSocketControlMessage(control)
 		if err != nil {
-			return nil, fmt.Errorf("Control message parse error: %s", err)
+			return Event{}, fmt.Errorf("Control message parse error: %s", err)
 		}
 		ev.scms = scms
 	}
@@ -118,12 +119,12 @@ func ReadEventUnix(fd int) (*Event, error) {
 	n, err = unix.Read(fd, data)
 	// n, err = c.conn.Read(data)
 	if err != nil {
-		return nil, err
+		return Event{}, err
 	}
 	if n != int(size)-8 {
-		return nil, fmt.Errorf("Invalid message size.")
+		return Event{}, fmt.Errorf("Invalid message size.")
 	}
-	ev.data = data
+	ev.Data = data
 	fmt.Println("data", data)
 	bytePool.Give(buf)
 	bytePool.Give(control)
@@ -131,31 +132,33 @@ func ReadEventUnix(fd int) (*Event, error) {
 	return ev, nil
 }
 
-func (c *Context) ReadEvent() (*Event, error) {
+func (c *Context) ReadEvent() (Event, error) {
 	buf := bytePool.Take(8)
 	control := bytePool.Take(24)
 
+	var ev Event
 	if c.fds == nil {
 		c.fds = make([]uintptr, 0)
 	}
 
 	n, oobn, _, _, err := unix.Recvmsg(c.SockFD, buf, control, unix.MSG_DONTWAIT)
 	if err != nil {
-		return nil, err
+		return ev, err
 	}
 	if n != 8 {
-		return nil, fmt.Errorf("Unable to read message header.")
+		return ev, fmt.Errorf("Unable to read message header.")
 	}
 
-	ev := new(Event)
+	// ev := new(Event)
+	// var ev Event
 
 	if oobn > 0 {
 		if oobn > len(control) {
-			return nil, fmt.Errorf("Unsufficient control msg buffer")
+			return ev, fmt.Errorf("Unsufficient control msg buffer")
 		}
 		scms, err := syscall.ParseSocketControlMessage(control)
 		if err != nil {
-			return nil, fmt.Errorf("Control message parse error: %s", err)
+			return ev, fmt.Errorf("Control message parse error: %s", err)
 		}
 		ev.scms = scms
 		fds, err := syscall.ParseUnixRights(&ev.scms[0])
@@ -176,12 +179,15 @@ func (c *Context) ReadEvent() (*Event, error) {
 	n, err = unix.Read(c.SockFD, data)
 	// n, err = c.conn.Read(data)
 	if err != nil {
-		return nil, err
+		return ev, err
 	}
 	if n != int(size)-8 {
-		return nil, fmt.Errorf("Invalid message size.")
+		return ev, fmt.Errorf("Invalid message size.")
 	}
-	ev.data = data
+	ev.Data = data
+	// fmt.Println(ev.Pid)
+	// fmt.Println(ev.Opcode)
+	// fmt.Println(data)
 	bytePool.Give(buf)
 	bytePool.Give(control)
 
@@ -245,7 +251,7 @@ func (ev *Event) Array() []int32 {
 }
 
 func (ev *Event) next(n int) []byte {
-	ret := ev.data[ev.off : ev.off+n]
+	ret := ev.Data[ev.off : ev.off+n]
 	ev.off += n
 	return ret
 }
